23df3881a1320f5ada3b5aec2e67853e59d449eb
Elouan <elouangauriaud@gmail.com>
2025-06-17 15:58:09 +0200

    Etape 3

--- Exercices_Individuels/3. Guess_A_Number/Guess_A_Number.js
+++ Exercices_Individuels/3. Guess_A_Number/Guess_A_Number.js
@@ -1,34 +1,56 @@
 // Etape 1:
 
-randomNumber = window.prompt("Veuillez choisir un nombre, s'il vous plaît !");
+// randomNumber = window.prompt("Veuillez choisir un nombre, s'il vous plaît !");
 
-let givenNumber = randomNumber;
+// let givenNumber = randomNumber;
 
-// Etape 2:
+// // Etape 2:
+
+// function didIWin(givenNumber) {
+//     if (givenNumber < 22) {
+//     return "Plus grand";
+//     } 
+//     else if (givenNumber > 22) {
+//     return "Plus petit";
+//     }
+//     else {
+//     return "Bravo ! Vous avez deviné le nombre";
+//     }
+// }
+
+// let result = givenNumber;
+
+// alert(didIWin(givenNumber))
+
+// // Etape 3:
+
+// function gamePlay(statement) {
+//     if(result == 22) {
+//         return True;
+//     } 
+//     else {
+//         return False;
+//     }
+// }
+
+// alert(gamePlay(result))
+
+// Etape 3:
+let randomNumber = window.prompt("Veuillez choisir un nombre, s'il vous plaît !");
 
 function didIWin(givenNumber) {
-    if (givenNumber < 22) {
-    return "Plus grand";
+    if (givenNumber == 22) {
+        return true;
     } 
-    else if (givenNumber > 22) {
-    return "Plus petit";
-    }
     else {
-    return "Bravo ! Vous avez deviné le nombre";
+        return false;
     }
 }
 
-let result = givenNumber;
-
-alert(didIWin(givenNumber))
-
-function gamePlay(statement) {
-    if(result == 22) {
-        return True;
-    } 
-    else{
-        return False;
+function gamePlay() {
+    while (didIWin(randomNumber) == false) {
+        randomNumber = window.prompt("Veuillez choisir un nombre, s'il vous plaît !");
     }
 }
 
-alert(gamePlay(result))
+gamePlay()
