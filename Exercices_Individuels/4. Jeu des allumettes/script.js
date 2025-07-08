614ddb0923967a9b192a2aa9182ce5382ad08692
Elouan <elouangauriaud@gmail.com>
2025-07-08 11:17:08 +0200

    Etape 1: Création de la fonction removeMatches

---
+++ Exercices_Individuels/4. Jeu des allumettes/script.js
@@ -0,0 +1,9 @@
+// Etape 1: 
+
+// On setup le nombre d'allumettes à 50 au départ, comme demandé
+let matches = 50;
+
+// On code une fonction qui servira à retirer un certain nombre d'allumettes selon le choix du joueur
+function removeMatches(numberOfMatchesToRemove) {
+
+}
