87b2e4ba73241d74489289c4f5a874483d6ffc1e
Elouan <elouangauriaud@gmail.com>
2025-07-11 15:55:28 +0200

    Etape 4: Ajout d'un dictionnaire inversé

--- Exercices_Individuels/5. Parlez vous le morse/script.js
+++ Exercices_Individuels/5. Parlez vous le morse/script.js
@@ -1,5 +1,9 @@
 // Etape 1: Création d'une fonction pour récupérer le paramètre et le mettre sous forme de tableau
 
+// --------------------------------------------------------------------------------------------------------------------------------------------- //
+// ------------------------------------------------------ LATIN -------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------------------------------------------------------- //
+
 function getLatinCharacterList(text) {
     return text.split('');
 }
@@ -36,7 +40,36 @@ const LATIN_TO_MORSE = {
 	'W':'.--',
 	'X':'-..-',
 	'Y':'-.--',
-	'Z':'--..'
+	'Z':'--..',
+    '0':'-----',
+    '1':'.----',
+    '2':'..---',
+    '3':'...--',
+    '4':'....-',
+    '5':'.....',
+    '6':'-....',
+    '7':'--...',
+    '8':'---..',
+    '9':'----.',
+    '.':'.-.-.-',
+    ',':'--..--',
+    '?':'..--..',
+    '!':'-.-.--',
+    '\'':'.----.',
+    '"':'.-..-.',
+    '(': '-.--.',
+    ' )':'-.--.-',
+    '&':'.-...',
+    '/':'-..-.',
+    ':':'---...',
+    ';':'-.-.-.',
+    '=':'-...-',
+    '+':'.-.-.',
+    '-':'-....-',
+    '_':'..--.-',
+    '$':'...-..-',
+    '@':'.--.-.',
+    ' ':'/'
 }
 
 function translateLatinCharacter(char) {
@@ -51,5 +84,66 @@ function encode(text) {
   return MORSE_CHARACTERS.join(' ');
 }
 
+// -------------------------------------------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------- MORSE -------------------------------------------------------------------------------- //
+// -------------------------------------------------------------------------------------------------------------------------------------------- //
+
+// Etape 4: Ajout d'un dictionnaire inversé
+
+// Permet d'inverser directement le dictionnaire latin même si l'on y applique des changements, le dictionnaire morse sera à jour
+const MORSE_TO_LATIN = {};
+for (let char in LATIN_TO_MORSE) {
+  MORSE_TO_LATIN[LATIN_TO_MORSE[char]] = char;
+}
+// const MORSE_TO_LATIN = {
+//   '-': "T",
+//   '--': "M",
+//   '---': "O",
+//   '--.': "G",
+//   '--.-': "Q",
+//   '--..': "Z",
+//   '-.': "N",
+//   '-.-': "K",
+//   '-.--': "Y",
+//   '-.-.': "C",
+//   '-..': "D",
+//   '-..-': "X",
+//   '-...': "B",
+//   '.': "E",
+//   '.-': "A",
+//   '.--': "W",
+//   '.---': "J",
+//   '.--.': "P",
+//   '.-.': "R",
+//   '.-..': "L",
+//   '..': "I",
+//   '..-': "U",
+//   '..-.': "F",
+//   '...': "S",
+//   '...-': "V",
+//   '....': "H"
+// }
+
+function getMorseCharacterList(morseText) {
+  return morseText.trim().split(' ');
+}
+
+function translateMorseCharacter(morseChar) {
+  return MORSE_TO_LATIN[morseChar] || '';
+}
+
+function decode(text) {
+  const WORDS = text.trim().split(' / ');
+  const DECODED_WORDS = WORDS.map(word => {
+    const SPACING = word.split(' ');
+    const LATIN_CHARACTERS = SPACING.map(translateMorseCharacter);
+    // Pas d’espace entre lettres d’un mot
+    return LATIN_CHARACTERS.join(''); 
+  });
+  // Espace entre mots
+  return DECODED_WORDS.join(' '); 
+}
+
 // Test
-console.log(encode("Hello, world"));
+console.log(encode("Hello, World!"));
+console.log(decode(".... . .-.. .-.. --- --..-- / .-- --- .-. .-.. -.. -.-.--"));
