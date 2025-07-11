55d23de6b097757f75e3190c1068b05df0dafb0d
Elouan <elouangauriaud@gmail.com>
2025-07-11 15:00:59 +0200

    Etape 2: Ajout d'un dictionnaire latin to morse

--- Exercices_Individuels/5. Parlez vous le morse/script.js
+++ Exercices_Individuels/5. Parlez vous le morse/script.js
@@ -7,3 +7,42 @@ function getLatinCharacterList(text) {
 const CHAIN_CHARACTER_LIST = "Hello, world";
 const RESULT = getLatinCharacterList(CHAIN_CHARACTER_LIST);
 console.log(RESULT);
+
+// Etape 2: Intégration du dictionnaire et test
+// Dictionnaire Latin to Morse
+const LATIN_TO_MORSE = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
+
+function translateLatinCharacter(char) {
+  const UPPER_CHARACTER = char.toUpperCase();
+  return LATIN_TO_MORSE[UPPER_CHARACTER] || ''; // Retourne une chaîne vide si le caractère n'existe pas
+}
+
+// Test
+console.log(translateLatinCharacter("A")); // Résultat attendu : .-
