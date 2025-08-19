15950e68a90ab6e575e44f31f57d9c4bb890d969
Elouan <elouangauriaud@gmail.com>
2025-08-19 15:53:28 +0200

    STEP 2: Creating USERS_TABLE

--- Exercices_Individuels/7. My_SQL/edusign.sql
+++ Exercices_Individuels/7. My_SQL/edusign.sql
@@ -1,7 +1,21 @@
+DROP TABLE IF EXISTS edusign;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE edusign (
     edusign_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    firstname VARCHAR (255) NOT NULL,
-    last_name VARCHAR (255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    user_id INTEGER,
     created_at DATETIME
-)
+);
+
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_firstname VARCHAR (255) NOT NULL,
+    user_last_name VARCHAR (255) NOT NULL,
+    user_email VARCHAR (255) UNIQUE
+);
+
+-- @block
+INSERT INTO users (user_firstname, user_last_name, user_email) VALUES ('Ada', 'Lovelace', 'ada@test.fr');
+INSERT INTO users (user_firstname, user_last_name, user_email) VALUES ('Beatrice', 'Worsley', 'bea@test.fr');
+INSERT INTO users (user_firstname, user_last_name, user_email) VALUES ('Bella', 'Guerin', 'bella@test.fr');
+INSERT INTO users (user_firstname, user_last_name, user_email) VALUES ('Barbara', 'Chase', 'barbara@test.fr');
