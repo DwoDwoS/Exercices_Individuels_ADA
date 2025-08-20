4928ba114aa2aedb17d64205202541318a4b7ac1
Elouan <elouangauriaud@gmail.com>
2025-08-20 10:41:44 +0200

    Fixing form

--- Exercices_Individuels/7. My_SQL/edusign.sql
+++ Exercices_Individuels/7. My_SQL/edusign.sql
@@ -24,29 +24,47 @@ INSERT INTO users (user_firstname, user_last_name, user_email) VALUES ('Barbara'
 SELECT * FROM users;
 
 -- @block
-SELECT * FROM users WHERE user_firstname="Ada";
+SELECT * FROM users 
+WHERE user_firstname="Ada";
 
 -- @block
-SELECT * FROM users WHERE user_firstname LIKE 'B%';
+SELECT * FROM users 
+WHERE user_firstname LIKE 'B%';
 
 -- @block
 SELECT COUNT(*) FROM users;
 
 -- @block
-SELECT COUNT(*) FROM users WHERE user_firstname LIKE 'B%';
+SELECT COUNT(*) FROM users 
+WHERE user_firstname LIKE 'B%';
 
 -- @block
 SELECT user_firstname FROM users;
 
 -- @block
-INSERT INTO edusign (user_id,created_at) SELECT user_id, "2024-05-30 09:30:00" FROM users WHERE user_firstname="Ada";
-INSERT INTO edusign (user_id,created_at) SELECT user_id, "2024-05-30 09:30:00" FROM users WHERE user_firstname="Bella";
-INSERT INTO edusign (user_id, created_at) SELECT user_id, "2024-09-01 09:30:00" FROM users;
+INSERT INTO edusign (user_id,created_at) 
+SELECT user_id, "2024-05-30 09:30:00" FROM users 
+WHERE user_firstname="Ada";
+
+INSERT INTO edusign (user_id,created_at) 
+SELECT user_id, "2024-05-30 09:30:00" FROM users 
+WHERE user_firstname="Bella";
+
+INSERT INTO edusign (user_id, created_at) 
+SELECT user_id, "2024-09-01 09:30:00" FROM users;
 
 -- @block
-SELECT * FROM edusign ORDER BY created_at DESC, user_id ASC;
-SELECT * FROM edusign WHERE created_at = "2024-05-30 09:30:00";
+SELECT * FROM edusign 
+ORDER BY created_at DESC, user_id ASC;
+
+SELECT * FROM edusign 
+WHERE created_at = "2024-05-30 09:30:00";
 
 -- @block
-SELECT created_at, COUNT(*) AS nb_users FROM edusign GROUP BY created_at ORDER BY created_at;
-SELECT COUNT(*) AS volume FROM edusign JOIN users ON edusign.user_id = users.user_id WHERE users.user_firstname="Bella";
+SELECT created_at, COUNT(*) AS nb_users FROM edusign 
+GROUP BY created_at 
+ORDER BY created_at;
+
+SELECT COUNT(*) AS volume FROM edusign 
+JOIN users ON edusign.user_id = users.user_id 
+WHERE users.user_firstname="Bella";
