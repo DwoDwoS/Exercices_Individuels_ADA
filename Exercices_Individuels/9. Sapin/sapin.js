c6ecccd059a679fdb8cd60d0de33d124edba362c
Elouan <elouangauriaud@gmail.com>
2025-09-18 16:10:06 +0200

    Etape 1.4

--- Exercices_Individuels/9. Sapin/sapin.js
+++ Exercices_Individuels/9. Sapin/sapin.js
@@ -6,20 +6,44 @@ function afficherEtoiles(n) {
   console.log(ligne);
 }
 
-function afficherTriangleDroite(n) {
+// function afficherTriangleDroite(n) {
+//   for (let i = 0; i < n; i++) {
+//     if (i === 0) {
+//       console.log('\\');
+//     } else {
+//       let ligne = '';
+//       for (let j = 0; j < i; j++) {
+//         ligne += '*';
+//       }
+//       ligne += '\\';
+//       console.log(ligne);
+//     }
+//   }
+// }
+
+
+function afficherTriangleGauche(n) {
   for (let i = 0; i < n; i++) {
-    if (i === 0) {
-      console.log('\\');
-    } else {
-      let ligne = '';
-      for (let j = 0; j < i; j++) {
+    let ligne = '';
+    
+    for (let j = 0; j < n - 1 - i; j++) {
+      ligne += ' ';
+    }
+    
+    ligne += '/';
+    
+    if (i > 0) {
+      for (let k = 0; k < i; k++) {
         ligne += '*';
       }
-      ligne += '\\';
-      console.log(ligne);
     }
+    
+    console.log(ligne);
   }
 }
 
-console.log("=== Tests afficherTriangleDroite ===");
-afficherTriangleDroite(5);
+console.log("=== Tests afficherTriangleGauche ===");
+afficherTriangleGauche(5);
+// console.log("=== Tests afficherTriangleDroite ===");
+// afficherTriangleDroite(5);
+
