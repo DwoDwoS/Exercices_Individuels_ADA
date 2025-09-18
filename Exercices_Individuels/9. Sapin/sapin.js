d9e08193804e7be9f2e84b4eec745b99902cdf9a
Elouan <elouangauriaud@gmail.com>
2025-09-18 16:32:20 +0200

    Etape 1.5

--- Exercices_Individuels/9. Sapin/sapin.js
+++ Exercices_Individuels/9. Sapin/sapin.js
@@ -1,49 +1,34 @@
-function afficherEtoiles(n) {
-  let ligne = '';
-  for (let i = 0; i < n; i++) {
-    ligne += '*';
+function afficherPointeSapin(hauteur) {
+  let espaces = '';
+  for (let i = 0; i < hauteur; i++) {
+    espaces += ' ';
   }
-  console.log(ligne);
-}
-
-// function afficherTriangleDroite(n) {
-//   for (let i = 0; i < n; i++) {
-//     if (i === 0) {
-//       console.log('\\');
-//     } else {
-//       let ligne = '';
-//       for (let j = 0; j < i; j++) {
-//         ligne += '*';
-//       }
-//       ligne += '\\';
-//       console.log(ligne);
-//     }
-//   }
-// }
-
-
-function afficherTriangleGauche(n) {
-  for (let i = 0; i < n; i++) {
+  console.log(espaces + '+');
+  
+  for (let i = 0; i < hauteur; i++) {
     let ligne = '';
     
-    for (let j = 0; j < n - 1 - i; j++) {
+    for (let j = 0; j < hauteur - 1 - i; j++) {
       ligne += ' ';
     }
     
     ligne += '/';
     
-    if (i > 0) {
-      for (let k = 0; k < i; k++) {
-        ligne += '*';
-      }
+    for (let k = 0; k < i; k++) {
+      ligne += '*';
     }
+    
+    ligne += '|';
+    
+    for (let k = 0; k < i; k++) {
+      ligne += '*';
+    }
+    
+    ligne += '\\';
     
     console.log(ligne);
   }
 }
 
-console.log("=== Tests afficherTriangleGauche ===");
-afficherTriangleGauche(5);
-// console.log("=== Tests afficherTriangleDroite ===");
-// afficherTriangleDroite(5);
-
+console.log("=== Tests afficherPointeSapin ===");
+afficherPointeSapin(4);
